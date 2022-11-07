2d5727c06cc928a04de2c9360edbc2b38ce63f4e
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-11-07 12:57:22 -0500

    NavBar

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -13,10 +13,10 @@ function NavBar() {
         <div className='q-full h-[90px] bg-black'>
             <div className='max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full'>
             <div>
-                <h1 className='text-white'>navbar</h1>
+                <h1 className='text-white'>Welcome to my portfolio!</h1>
             </div>
-            <div className='flex text-white items-center'>
-                <ul>
+            <div className='hidden md:flex'>
+                <ul className='flex text-white items-center'>
                     <li>About Me</li>
                     <li>Skills</li>
                     <li>Projects</li>
@@ -28,6 +28,17 @@ function NavBar() {
 
             <div onClick={handleNav} className='block md:hidden'>
                 {nav ? <AiOutlineClose size={30} className='text-white' /> : <AiOutlineMenu size={30} className='text-white' />}
+            </div>
+
+            {/* Mobile Menu */}
+
+            <div className={nav ? 'w-full bg-black text-white absolute top-[90px] left-o flex justify-center text-center' : 'absolute left-[-100%]'}>
+                <ul>
+                    <li>About Me</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact Me</li>
+                </ul>
             </div>
             </div>
            
