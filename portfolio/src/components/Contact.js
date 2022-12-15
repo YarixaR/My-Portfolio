7e620bc12604bf7ccb22a1357edba7545f3f37e9
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-12-14 22:36:22 -0500

    added picture of taro

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { FaLinkedin } from 'react-icons/fa'
+import taro from '../assets/taro.jpg'
+
 
 
 function Contact() {
     return(
-        <div className="bg-orange-50 text-center pt-10 pb-10">
+        <div className="bg-orange-50 text-center pt-10 pb-10 flex justify-center ">
             <section id='contact'>
+                <img src={taro} alt='taro the cat' className="w-40 h-50 ml-12"/>
                 <h1>Contact Me</h1>
                 <p>
                     <a href="mailto:yarixarodriguez94@gmail.com">Yarixarodriguez94@gmail.com</a>
