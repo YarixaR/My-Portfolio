1bcfb68670f1dba26a595b83eb4324117d62e1b8
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-12-21 15:13:24 -0500

    centered about me text

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -12,14 +12,14 @@ function Header() {
 
                 <div className=" md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
                     {isAboveMediumScreens ? (
-                        <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+                        <div className="relative z-0 ml-10 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
                             <img className="hover:filter hover:saturate-150 transition duration-500 z-10 w-80 rounded-t-[400px]  max-w-[400px] md:max-w-[600px] " src={image} alt='profile'/>
                         </div>
                     ) : (
                         <img className="hover:filter hover:saturate-150 transition duration-500 z-10 w-80 rounded-full max-w-[400px] md:max-w-[600px]" src={image} alt='profile'/>
                     )}
                 </div>
-                    <div className="z-30 basis-2/5 mt-12 md:mt-32 text-center">
+                    <div className="z-30 basis-2/5 mt-12 md:mt-32 text-center ml-20">
                     <h1 >About Yarixa</h1>
                         <p >Hi! I'm Yari, and I'd love to share a few things to get to know me a little more. I have a passion for animals, cooking, drinking matcha and coffee, playing video games, and traveling. This year, I've decided to embark on a new journey and strive to make a career change. I've dedicated numerous years healing and nourishing animals to facilitate people's lives but now I'm excited to start focusing on my creative side by ideating solutions, sketching them out, and bringing them to life.  
                         </p>
