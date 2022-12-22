a8e0256842c055394b352c2f5710790b3475ee6f
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-12-22 17:44:50 -0500

    fixed margin

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -19,7 +19,7 @@ function Header() {
                         <img className="hover:filter hover:saturate-150 transition duration-500 z-10 w-80 rounded-full max-w-[400px] md:max-w-[600px]" src={image} alt='profile'/>
                     )}
                 </div>
-                    <div className="z-30 basis-2/5 mt-12 md:mt-32 text-center ml-20">
+                    <div className="z-30 basis-2/5 mt-12 md:mt-32 md:ml-10 text-center ">
                     <h1 >About Yarixa</h1>
                         <p >Hi! I'm Yari, and I'd love to share a few things to get to know me a little more. I have a passion for animals, cooking, drinking matcha and coffee, playing video games, and traveling. This year, I've decided to embark on a new journey and strive to make a career change. I've dedicated numerous years healing and nourishing animals to facilitate people's lives but now I'm excited to start focusing on my creative side by ideating solutions, sketching them out, and bringing them to life.  
                         </p>
